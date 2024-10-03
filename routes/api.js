ce51408d961c74f1a7937718f830b9519a378a54
Kais Bitar <k.bitar@sensory-minds.com>
2024-10-03 15:39:10 +0100

    Pushing before making changes

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const gloablDir = process.cwd();
+const axios = require("axios");
+const https = require("https");
+const agent = new https.Agent({ rejectUnauthorized: false });
 
-const express = require('express')
-const router = express.Router()
-const gloablDir = process.cwd()
+router.get("/api/quran-model", (req, res) => {
+  res.sendFile("./storage/deliverables/quranModel.json", { root: gloablDir });
+});
+router.get("/api/quran-index", (req, res) => {
+  res.sendFile("./storage/deliverables/quranIndexModel.json", {
+    root: gloablDir,
+  });
+});
+router.get("/api/with-tashkeel", (req, res) => {
+  res.sendFile("./storage/resources/allVersesWithTashkeel.json", {
+    root: gloablDir,
+  });
+});
+router.get("/api/view/sura-details/:fileName", (req, res) => {
+  res.sendFile(
+    `./storage/deliverables/suras-details/${req.params.fileName}.json`,
+    { root: gloablDir }
+  );
+});
 
+const scrapeWebsite = require("./test");
+router.get(`/api/scrape/:word`, async (req, res) => {
+  const word = req.params.word;
+  // const url = `http://www.baheth.info/find/web/${word}?sources=القاموس المحيط`;
+  const url = `https://www.arabicterminology.com/?search=${word}&book=معجم الغني`;
+  //www.baheth.info/find/web/${word}?sources=القاموس المحيط
+  //   const url = `https://www.almaany.com/ar/dict/ar-ar/${word}`;
+  http: try {
+    const data = await scrapeWebsite(url);
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching the website" });
+  }
+});
 
-router.get('/api/quran-model', (req, res) => {
-    res.sendFile('./storage/deliverables/quranModel.json', { root: gloablDir });
-})
-router.get('/api/quran-index', (req, res) => {
-    res.sendFile('./storage/deliverables/quranIndexModel.json', { root: gloablDir })
-})
-router.get('/api/with-tashkeel', (req, res) => {
-    res.sendFile('./storage/resources/allVersesWithTashkeel.json', { root: gloablDir })
-})
-router.get('/api/view/sura-details/:fileName', (req, res) => {
-    // res.send(req.params.fileName)
-    res.sendFile(`./storage/deliverables/suras-details/${req.params.fileName}.json`, { root: gloablDir })
-})
-
-
-module.exports = router
+module.exports = router;
--- routes/services.js
+++ routes/services.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const gloablDir = process.cwd()
+const express = require("express");
+const router = express.Router();
+const gloablDir = process.cwd();
 
-
-
-router.get('/', (req, res) => {
-    res.sendFile('./views/home.html', { root: gloablDir })
+router.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: gloablDir });
 });
 
-router.get('/services/run', (req, res, next) => {
-    require('../services/resourcesService.js')
-    // res.send('<a href="/">Home</a>')
-    res.writeHead(302, {
-        'Location': '/'
-      });
-    res.end();
-})
+router.get("/services/run", (req, res, next) => {
+  require("../services/resourcesService.js");
+  // res.send('<a href="/">Home</a>')
+  res.writeHead(302, {
+    Location: "/",
+  });
+  res.end();
+});
 
-module.exports = router
+module.exports = router;
